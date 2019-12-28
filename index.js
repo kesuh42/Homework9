23ce04136e7bdf628a7f81a5608a7b661cc9debd
Kevin Suh <kevinsuh@Kevins-MacBook-Pro.local>
2019-12-28 11:31:37 -0600

    axios call is in, need to construct html now

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var fs = require("fs")
 var pdf = require('html-pdf');
+var axios = require("axios")
+var inquirer = require("inquirer")
 
 var templateHTML = `
 <!DOCTYPE html>
@@ -14,7 +16,25 @@ var templateHTML = `
     </html>
 `
 
+inquirer.prompt([
+    {
+        type: "input",
+        name: "github",
+        message: "What is your github url?"
+    },
+    {
+        type: "rawlist",
+        name: "color",
+        message: "What is your favorite color?",
+        choices: ["red", "blue", "yellow", "green", "purple", "orange", "pink"]
+    }
+]).then(function(data){
+    axios.get(`https://api.github.com/users/${data.github}`).then(function(data){
+        console.log(data)
+    })
+    
+})
+
 pdf.create(templateHTML).toFile('index.pdf', function(err, res) {
     if (err) return console.log(err);
-    console.log(res); // { filename: '/app/businesscard.pdf' }
   });
